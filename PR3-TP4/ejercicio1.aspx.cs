afb638940d1e5aafefcdbf68ad7b03be2cef9426
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-04-25 22:32:23 -0300

    Agregue base de datos viajes para el codigo

--- PR3-TP4/ejercicio1.aspx.cs
+++ PR3-TP4/ejercicio1.aspx.cs
@@ -13,7 +13,26 @@ namespace PR3_TP4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack == false) {
             
+            SqlConnection cn = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=Viajes;Integrated Security=True");
+       
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("Select * from Provincias", cn);
+         
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            ddlProvInicio.DataSource = dr;
+
+            ddlProvInicio.DataTextField = "NombreProvincia";
+            ddlProvInicio.DataValueField = "IdProvincia";
+
+            ddlProvInicio.DataBind();
+
+
+            cn.Close();
+        }
         }
     }
 }
