c39152b519350f1e350edb8074ed5473c2be8c6c
NicoleLigoule <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-04-28 17:53:28 -0300

    Ejercicio 2: Agregue file ejercicio2.aspx, label IdProducto, dropdownlist IdProducto y la conexion a la db Neptuno

---
+++ PR3-TP4/ejercicio2.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ejercicio2.aspx.cs" Inherits="PR3_TP4.ejercicio2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <asp:Label runat="server" Text="Id Producto:" ID="IdProducto"></asp:Label>
+            <asp:DropDownList runat="server" ID="ddlProducto"></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PR3-TP4/ejercicio2.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace PR3_TP4
+{
+    public partial class ejercicio2 : System.Web.UI.Page
+    {
+        private SqlConnection cn = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Clientes", cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+        }
+    }
+}
